5beb1fe0df2be26dbd8d8a08d3b2906677b23e15
AdamN3wTS <adamjanus2004@gmail.com>
2025-04-24 20:15:18 +0200

    Added MagazynViews and MagazynAddItemController es?

--- WarsztatApp.Web/Controllers/WarsztatController.cs
+++ WarsztatApp.Web/Controllers/WarsztatController.cs
@@ -27,7 +27,90 @@ namespace WarsztatApp.Web.Controllers
             var warsztat = await _appDbContext.Warsztaty.FirstOrDefaultAsync(w => w.UserId == user.Id);
             return View(warsztat);
         }
-        
 
+        [HttpGet]
+        public async Task<IActionResult> Profil()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            var warsztat = await _appDbContext.Warsztaty.FirstOrDefaultAsync(w => w.UserId == user.Id);
+            if (warsztat == null)
+            {
+                return View("Error");
+            }
+
+            return View(warsztat);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Profil(Warsztat warsztat)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return View("Error");
+                }
+                var warsztatDb = await _appDbContext.Warsztaty.FirstOrDefaultAsync(w => w.UserId == user.Id);
+                warsztatDb.Adres = warsztat.Adres;
+                warsztatDb.Nazwa = warsztat.Nazwa;
+                await _appDbContext.SaveChangesAsync();
+                return RedirectToAction("Home", "Warsztat");
+            }
+            return View(warsztat);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Magazyn()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return View("Error");
+            }
+            var warsztat = await _appDbContext.Warsztaty.Include(w => w.Magazyn).ThenInclude(m => m.Przedmioty).FirstOrDefaultAsync(w => w.UserId == user.Id);
+            return View(warsztat.Magazyn);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DodajPrzedmiot(Przedmiot przedmiot)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return View("Error");
+
+                }
+                var warsztatDb = await _appDbContext.Warsztaty.Include(w => w.Magazyn).ThenInclude(m => m.Przedmioty).FirstOrDefaultAsync(w => w.UserId == user.Id);
+                Przedmiot przedmiotDb = new Przedmiot
+                {
+                    Nazwa = przedmiot.Nazwa,
+                    Ilosc = przedmiot.Ilosc,
+                    Cena = przedmiot.Cena,
+                    MagazynId = warsztatDb.Magazyn.Id,
+                    typPrzedmiotu = przedmiot.typPrzedmiotu,
+                };
+                await _appDbContext.Przedmioty.AddAsync(przedmiotDb);
+                
+                await _appDbContext.SaveChangesAsync();
+                return RedirectToAction("Magazyn");
+            }
+            return View(przedmiot);
+        }
+        [HttpGet]
+        public async Task<IActionResult> DodajPrzedmiot() 
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return View("Error");
+            }
+            var warsztat = await _appDbContext.Warsztaty.Include(w => w.Magazyn).ThenInclude(m => m.Przedmioty).FirstOrDefaultAsync(w => w.UserId == user.Id);
+            return View(new Przedmiot());
+        }
     }
 }
